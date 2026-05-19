406d2abbc2b07dc832d467bc48053dda80fa7022
Sch <junhg0211@gmail.com>
2026-05-19 14:23:15 +0900

    Add typography styles and container layout to pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,34 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<style>
+		body {
+			font-familiy:
+				-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+		}
+	</style>
 </svelte:head>
 
-{@render children()}
+<div class="container">
+	<header>
+		<h1>OECES</h1>
+		<p>Online Encyclopedia of Constructed language Example Sentences</p>
+		<form method="get" action="/search">
+			<input type="text" name="q" />
+			<button type="submit">Search</button>
+		</form>
+	</header>
+	{@render children()}
+</div>
+
+<style>
+	.container {
+		max-width: 1000px;
+		margin: 0 auto;
+	}
+
+	header {
+		text-align: center;
+		margin-bottom: 16px;
+	}
+</style>
---
+++ src/routes/[sid]/+page.svelte
@@ -0,0 +1,68 @@
+<div class="sentence">
+	<div class="sentence-header">
+		<div class="sentence-id">S000001</div>
+		<div class="sentence-content">This is a sentence.</div>
+	</div>
+	<table>
+		<tbody>
+			<tr>
+				<td>언어</td>
+				<td>자소크어</td>
+			</tr>
+			<tr>
+				<td>댓글</td>
+				<td>와하하 댓글이래요 -- 스치, 2026. 5. 19</td>
+			</tr>
+			<tr>
+				<td>출처</td>
+				<td>내가씀 (이럴 때에는 출처를 딱히 안 써도 돼요)</td>
+			</tr>
+			<tr>
+				<td>참고</td>
+				<td>
+					참고로 볼 수 있을만한 글이나... 링크같은 거 적어놓으면 좋을 듯하요. 음하하 참고로 볼 수
+					있을만한 글이나... 링크같은 거 적어놓으면 좋을 듯하요.
+				</td>
+			</tr>
+			<tr>
+				<td>키워드</td>
+				<td>욕심 많은 광부 이야기, 사트</td>
+			</tr>
+			<tr>
+				<td>저자</td>
+				<td>자소크 스치</td>
+			</tr>
+			<tr>
+				<td>날짜</td>
+				<td>2026. 5. 19</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+<style>
+	.sentence {
+		border-top: 1px solid black;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.sentence-header {
+		display: flex;
+		background-color: #eeeeff;
+	}
+
+	.sentence-id {
+		width: 100px;
+	}
+
+	table {
+		padding-left: 16px;
+	}
+
+	tr td:first-child {
+		min-width: 120px;
+		font-size: 12px;
+		vertical-align: top;
+	}
+</style>
